c078710d3f4b39f224ed8acbec55a52f60548283
KevinKons <kevin.kons@hotmail.com>
2019-03-12 11:19:09 -0300

    criando projeto

---
+++ project/src/main/java/com/file/upload_and_download/UploadAndDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.file.upload_and_download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadAndDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadAndDownloadApplication.class, args);
+    }
+
+}
---
+++ project/src/test/java/com/file/upload_and_download/UploadAndDownloadApplicationTests.java
@@ -0,0 +1,16 @@
+package com.file.upload_and_download;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UploadAndDownloadApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
